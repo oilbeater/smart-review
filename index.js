7347f27690942d23e01cb62f861b4b16a5c0341e
oilbeater <liumengxinfly@gmail.com>
2023-03-23 16:15:16 +0000

    basic work

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const axios = require('axios');
+import core from '@actions/core';
+import github from '@actions/github';
+import axios from 'axios';
+import { ChatGPTAPI } from 'chatgpt';
+import fetch from 'node-fetch';
 
 async function run() {
     try {
@@ -9,7 +11,23 @@ async function run() {
         core.info('pr patch url is ' + patchUrl)
         const response = await axios.get(patchUrl);
         const patchContent = response.data;
-        core.debug('PR Patch Content: ' + patchContent);
+        core.info('pr patch data is ' + patchContent.length)
+        const chatAPI = new ChatGPTAPI({
+            apiKey: '',
+            completionParams: {
+                model: 'gpt-3.5-turbo',
+                temperature: 0.1,
+                top_p: 1,
+            },
+            fetch: fetch,
+        });
+        const systemMessage = 'You are a professional Golang programmer reviewing the code patch and giving feedbacks focus only on potential bugs, format errors, if the commit message is enough to describe this patch and ways to improve.';
+
+        core.info('start send')
+        console.time('code-review cost');
+        const res = await chatAPI.sendMessage(patchContent, {systemMessage: systemMessage});
+        console.timeEnd('code-review cost');
+        core.info(res.text);
     } catch (error) {
         core.setFailed(error.message);
     }
